e04fb8d9b1d2cdbbc0cca3f70ff8fa695085320f
Chris Moreton <chris_gh@netsensia.com>
2013-09-13 17:28:44 +0100

    Add CH module

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     'modules' => array(
         'Application',
         'Netsensia',
-        'ZendSentry','CompaniesHouse'
+        'ZendSentry',
+        'NetsensiaCompanies'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
