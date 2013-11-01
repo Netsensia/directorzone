f5313cc04a8dc50d70209376e23cc552d5334a98
Chris Moreton <chris@netsensia.com>
2013-11-01 15:52:02 +0000

    Companies House

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -2,11 +2,17 @@
 namespace Application\Controller;
 
 use Netsensia\Controller\NetsensiaActionController;
+use NetsensiaCompanies\Search\Companies;
 
 class IndexController extends NetsensiaActionController
 {
     public function indexAction()
     {
+        $c = $this->getServiceLocator()->get('NetsensiaCompanies\Search\Companies');
+        
+        echo($c->test());
+        die;
+        
        return [
             'flashMessages' => $this->getFlashMessages(),
        ];
