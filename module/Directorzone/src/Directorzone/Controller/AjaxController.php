6dc17ea540a622700121c8aa31ab68fbb4a88daa
Chris Moreton <chris@netsensia.com>
2013-11-13 21:47:35 +0000

    Navigate company types

--- module/Directorzone/src/Directorzone/Controller/AjaxController.php
+++ module/Directorzone/src/Directorzone/Controller/AjaxController.php
@@ -4,6 +4,7 @@ namespace Directorzone\Controller;
 
 use Netsensia\Controller\NetsensiaActionController;
 use Zend\Mvc\MvcEvent;
+use Zend\View\Model\JsonModel;
 
 class AjaxController extends NetsensiaActionController
 {
@@ -15,6 +16,29 @@ class AjaxController extends NetsensiaActionController
 
     public function companySearchAction()
     {
+        $page = $this->params()->fromQuery('page', null);
+        $size = $this->params()->fromQuery('size', null);
+        
+        $start = ($page - 1) * $size + 1;
+        $end = $start + $size;
+        
+        $companies = [
+            'results' => [],
+            'total' => 100,	
+        ];
+        
+        for ($i=$start; $i<=$end; $i++) {
+            $companies['results'][] = [
+	           'number' => $i,
+	           'name' => 'Company number ' . $i,
+	           'ceo' => 'Barry Jones',
+	           'sector' => 'Electricals'
+            ];
+        }
+        
+        return new JsonModel(
+	        $companies
+        );
     }
 
 }
