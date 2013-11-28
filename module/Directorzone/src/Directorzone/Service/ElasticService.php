89abdd0d5da4a57b1962ae99b02262cebf0d40c5
Chris Moreton <chris@netsensia.com>
2013-11-28 18:56:46 +0000

    Solve ampersand problem

--- module/Directorzone/src/Directorzone/Service/ElasticService.php
+++ module/Directorzone/src/Directorzone/Service/ElasticService.php
@@ -21,24 +21,51 @@ class ElasticService extends NetsensiaService
     }
     
     public function search($name)
-    {
+    {        
         $params = [
             'index' => 'companies',
-            'type'  => 'company'
+            'type'  => 'company',
         ];
         
-        $params['body']['query']['match']['name'] = $name;
+        $params['body']['query']['query_string']['query'] = $name;
+        $params['body']['query']['query_string']['default_field'] = 'name';
+        $params['body']['query']['query_string']['default_operator'] = 'OR';
+        $params['body']['query']['query_string']['analyzer'] = 'standard';
         
         $result = $this->client->search($params);
         
         return $result;
     }
     
+    private function createCompaniesIndex()
+    {
+        $indexParams['index']  = 'companies';
+        
+        // Index Settings
+        $indexParams['body']['settings']['number_of_shards']   = 3;
+        $indexParams['body']['settings']['number_of_replicas'] = 2;
+        
+        $indexParams['body']['settings']['analysis']['char_filter']['nopunc_mapping'] = [
+            "type" => "mapping",
+            "mappings" => [".=>"]
+        ];
+        
+        $indexParams['body']['settings']['analysis']['analyzer']['custom_with_char_filter'] = [
+            "tokenizer" => "standard",
+            "char_filter" => ["nopunc_mapping"]
+        ];
+       
+        // Create the index
+        $this->client->indices()->create($indexParams);
+    }
+    
     public function indexCompanies()
     {
+        $this->createCompaniesIndex();
+        
         $this->client->indices()->delete(array(array('index' => 'companies')));
         
-        $limit = 5000;
+        $limit = 10000;
         $lastCompanyId = -1;
         
         $count = 0;
@@ -46,7 +73,9 @@ class ElasticService extends NetsensiaService
             
             $rowset = $this->companyTableGateway->select(
                 function (Select $select) use ($limit, $lastCompanyId) {
-                    $select->order('companyid ASC')->limit($limit)->where->greaterThan('companyid', $lastCompanyId);
+                    $select->order('companyid ASC')
+                           ->limit($limit)
+                           ->where->greaterThan('companyid', $lastCompanyId);
                 }
             );
             
@@ -55,6 +84,17 @@ class ElasticService extends NetsensiaService
             $body = '';
             
             foreach ($rowset as $row) {
+                
+                if (isset($row['incorporationdate'])) {
+                    if (empty($row['incorporationdate']) || 
+                        $row['incorporationdate'] == 'Unknown') {
+                        unset($row['incorporationdate']);
+                        //echo 'Unset' . PHP_EOL;
+                    } else {
+                        //echo 'DATE: ' . $row['incorporationdate'] . PHP_EOL;
+                    }
+                }
+                
                 $count ++;
                 
                 $found = true;
@@ -77,9 +117,11 @@ class ElasticService extends NetsensiaService
                 
                 $result = $this->client->bulk($document);
                 
-                if (isset($result['error'])) {
-                    var_dump($result);
-                    die;
+                for ($i=0; $i<5000; $i++) {
+                    if (isset($result['items'][$i]['index']['error'])) {
+                        var_dump($result['items'][$i]['index']);
+                        die;
+                    }
                 }
                 
                 echo $count . ' | ' . $row['name'] . PHP_EOL;
