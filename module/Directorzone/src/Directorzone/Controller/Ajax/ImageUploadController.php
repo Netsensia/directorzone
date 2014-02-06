543e5bee6c30b0a9bd5f9c8701e5ef62be3e4812
Chris Moreton <chris@netsensia.com>
2014-02-06 18:42:58 +0000

    WIP image uploads

--- module/Directorzone/src/Directorzone/Controller/Ajax/ImageUploadController.php
+++ module/Directorzone/src/Directorzone/Controller/Ajax/ImageUploadController.php
@@ -22,12 +22,29 @@ class ImageUploadController extends NetsensiaActionController
     
     public function imageUploadAction()
     {
-        return new JsonModel(
-            [
-                'teaser'=>'/img/flag/England.fw.png',
-                'thumb' => '/img/flag/Germany.fw.png',
-                'main' => '/img/flag/Italy.fw.png',
-            ]
-        );
+        return JsonModel($_FILES);
+        
+        if ($_FILES && isset($_FILES[0])) {
+            file_put_contents('test.txt', print_r($_FILES, true));
+            
+            $file = $_FILES[0];
+            $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+            $filename = time() . '_' . uniqid() . '_' . md5($file['name']) . '.' . $extension;
+            $finalPath = 'img/upload/' . $filename;
+            move_uploaded_file($file['tmp_name'], 'public/' . $finalPath);
+            file_put_contents('test2.txt', $finalPath);
+            
+            //{"account-publish-image":{"name":"enhanced-buzz-30227-1367984436-3.jpg","type":"image/jpeg","tmp_name":"/tmp/phpVs5F8l","error":0,"size":126392}}
+            return new JsonModel(
+                [
+                    'teaser'=>'/img/flag/England.fw.png',
+                    'thumb' => '/' . $finalPath,
+                    'main' => '/img/flag/Italy.fw.png',
+                ]
+            );
+        } else {
+            $this->getResponse()->setStatusCode(500);
+        }
     }
 }
+?>
