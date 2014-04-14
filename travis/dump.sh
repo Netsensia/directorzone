f832ec3e79129edcffb5ae9632f1b3456bc1c74f
Chris Moreton <chris@netsensia.com>
2014-04-14 15:09:56 +0100

    Update DB dump script

--- travis/dump.sh
+++ travis/dump.sh
@@ -1,5 +1,5 @@
-mysqldump -uroot -p --add-drop-database --disable-keys --databases directorzone_zf2 > ../../db_create.sql
-cd ../..
+mysqldump -uroot -p --add-drop-database --disable-keys --databases directorzone_zf2 > /home/cmoreton/db_create.sql
+cd /home/cmoreton
 rm db_create.zip
 zip db_create.zip db_create.sql
 rm db_create.sql
