99c0170c3d7cebad10e205c590a457df2f463b28
Chris Moreton <chris@netsensia.com>
2014-07-01 11:26:09 +0100

    Send message dialog box

--- travis/dump.sh
+++ travis/dump.sh
@@ -3,7 +3,7 @@
 if [ `hostname -s` = "vagrant" ]; then 
   exit 0
 else
-  mysqldump -uroot -p --add-drop-database --disable-keys --databases directorzone_zf2 > /home/cmoreton/db_create.sql
+  mysqldump -uroot --add-drop-database --disable-keys --databases directorzone_zf2 > /home/cmoreton/db_create.sql
   cd /home/cmoreton
   rm db_create.zip
   zip db_create.zip db_create.sql
