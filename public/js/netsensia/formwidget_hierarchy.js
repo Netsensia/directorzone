6f9988af1906376aae066b1b66bcaa6aa970c184
Chris Moreton <chris@netsensia.com>
2014-09-17 09:34:43 +0100

    Widget selects

--- public/js/netsensia/formwidget_hierarchy.js
+++ public/js/netsensia/formwidget_hierarchy.js
@@ -31,27 +31,53 @@ $(document).ready(function() {
 		return items;
 	}
 	
-	$("input[id^=netsensiaWidget_hierarchy]").each(function() {
-    	var widgetId = $(this).attr('id');
-    	var allOptions = $.parseJSON($(this).attr('data-hierarchyvalues'));
-    	var widgetValue = $.parseJSON($(this).val());
-    	var parentId = -1;
-    	var selectOptions = getOptions(allOptions, parentId);
-    	var widgetDiv = $('div[data-netsensia-group=' + widgetId + '] div.controls');
-    	var labelDiv = $('div[data-netsensia-group=' + widgetId + '] label.control-label');
-    	$(labelDiv).html(widgetValue.label);
-    	$(widgetDiv).append('<div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></div>');
-    	var widgetDiv = $('div[data-netsensia-group=' + widgetId + '] div.controls div.input-group');
-    	
-    	var selectHtml = '<select id="' + widgetId + '_parentId" class="form-control">';
+	function getSelectHtml(widgetId, parentId, allOptions)
+	{
+		var selectOptions = getOptions(allOptions, parentId);
+		
+    	var selectHtml = '<select id="' + widgetId + '_' + parentId + '" data-widgetId="' + widgetId + '">';
+    	selectHtml += '<option value="-1">Please select...</option>';
+    	var count = 0;
     	for (var i=0; i<selectOptions.length; i++) {
     		selectHtml += '<option value="' + selectOptions[i][0] + '">' + selectOptions[i][1] + '</option>';
+    		count++;
     	}
     	selectHtml += '</select>';
     	
-    	$(widgetDiv).append(selectHtml);
+    	return count > 0 ? selectHtml : '';
+	}
+	
+	$('input[id^=netsensiaWidget_hierarchy]').each(function() {
+		
+		var widgetId = $(this).attr('id');
+    	var allOptions = $.parseJSON($(this).attr('data-hierarchyvalues'));
+    	var widgetValue = $.parseJSON($(this).val());
+    	
+    	var widgetDiv = $('div[data-netsensia-group=' + widgetId + '] div.controls');
+    	var labelDiv = $('div[data-netsensia-group=' + widgetId + '] label.control-label');
+    	
+    	$(labelDiv).html(widgetValue.label);
+    	$(widgetDiv).append('<div class="input-group">');
+    	var widgetDiv = $('div[data-netsensia-group=' + widgetId + '] div.controls div.input-group');
+    	
+    	$(widgetDiv).append(getSelectHtml(widgetId, -1, allOptions));
 
     });
+	
+	$(document).delegate('select[id^=netsensiaWidget_hierarchy]', 'change', function() {
+		
+		$(this).nextAll('select').each(function() {
+			$(this).remove();
+		})
+		
+		var widgetId = $(this).attr('data-widgetId');
+		var widgetEl = $('input#' + widgetId);
+		var allOptions = $.parseJSON($(widgetEl).attr('data-hierarchyvalues'));
+		var widgetDiv = $('div[data-netsensia-group=' + widgetId + '] div.controls div.input-group');
+		var selectedId = $(this).val();
+		$(widgetDiv).append(getSelectHtml(widgetId, selectedId, allOptions));
+		
+	});
 	
 });
 
