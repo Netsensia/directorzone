fdb57f8385e2e6d4b17dd7ca88005ace6ec7dc78
Chris Moreton <chris@netsensia.com>
2014-11-26 10:22:24 +0000

    Home page windows

--- module/Netsensia/src/Netsensia/View/Helper/NewsPanel.php
+++ module/Netsensia/src/Netsensia/View/Helper/NewsPanel.php
@@ -17,12 +17,12 @@ class NewsPanel extends AbstractHelper
     public function __invoke($panelTitle, array $items, $panelClass = 'panel-default')
     {
         ?>
-            <div class="panel news-panel <?php echo $panelClass; ?>">
-            <div class="panel-heading">
-            <h3 class="panel-title">
+<div class="panel news-panel <?php echo $panelClass; ?>">
+	<div class="panel-heading">
+		<h3 class="panel-title">
             <?php echo $panelTitle; ?>
             </h3>
-            </div>
+	</div>
         <?php
         $count = 0;
         foreach ($items as $item) {
@@ -32,35 +32,35 @@ class NewsPanel extends AbstractHelper
             $bbCodeParser->parse($content);
             $content = $bbCodeParser->getAsHtml();
             $content = $this->stripBBCode($content);
-
-        ?>
+            ?>
 
                 <?php if (++$count == 1): ?>
-                            <div class="panel-body">
-                
-                                <div class="media">
-                
-                    <a class="pull-left" href="#">
-                    <img class="media-object" style="height:45px; width:45px" src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>">
-                    </a>
-                    <div class="media-body">
-                    <h4 class="media-heading"><a href="/article/<?php echo $item['articleid']; ?>"><?php echo $item['title']; ?></a></h4>
-                    <div style="max-height:6em"><?php echo $content; ?></div>
-                    </div>
-                                    </div>
-                                                </div>
+                            
+        <div class="panel-body" style="overflow:hidden">
+    		<div class="media">
+    			<div style="margin-bottom:0.5em;background: #fff url(<?php echo $item['image']; ?>) center center no-repeat;width: 100%;height:100px;border: 1px solid #fff;text-indent: -9999px;">
+    			</div>
+    			<div class="media-body">
+    				<h4 class="media-heading" style="font-size: 1.15em">
+    					<a href="/article/<?php echo $item['articleid']; ?>"><?php echo $item['title']; ?></a>
+    				</h4>
+                        <?php $fontSize=0.86; ?>
+                        <div style="max-height:4em;font-size:<?php echo $fontSize; ?>em"><?php echo $content; ?></div>
+    			</div>
+    		</div>
+	    </div>
                                     
                 <?php else: ?>
-                <div class="mini-list-item">
-                    <a href="/article/<?php echo $item['articleid']; ?>"><?php echo $item['title']; ?></a>
-                    </div>
+                <div class="mini-list-item" style="max-height: 1.5em;overflow:hidden">
+		<a href="/article/<?php echo $item['articleid']; ?>"><?php echo $item['title']; ?></a>
+	</div>
                 <?php endif ?>
 
         <?php
         }
         ?>
         </div>
-        <?php
+<?php
         
     }
 }
