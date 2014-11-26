7c81ddd202fbf3f305c2ad77f0e32eb8a72628ce
Chris Moreton <chris@netsensia.com>
2014-11-26 16:01:15 +0000

    Tidy article display

--- module/Directorzone/src/Directorzone/View/Helper/ArticleAuthor.php
+++ module/Directorzone/src/Directorzone/View/Helper/ArticleAuthor.php
@@ -18,7 +18,7 @@ class ArticleAuthor extends AbstractHelper
     	if ($isAnonymous) {
     	    echo 'This is published by a Directorzone member. To make contact with the author, please login as a member and click reply.';
     	} else {
-    	    echo 'Published by ' . $this->view->name;
+    	    echo 'Published by ' . $this->view->name . ' on ' . date("F j, Y, g:i a", strtotime($this->view->publishdate));
     	}
 
     }
