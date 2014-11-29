6e387437e090e6193382ca5fbee401ca07de5601
Chris Moreton <chris@netsensia.com>
2014-11-29 12:23:53 +0000

    Strip tags for news panels

--- module/Netsensia/src/Netsensia/View/Helper/NewsPanel.php
+++ module/Netsensia/src/Netsensia/View/Helper/NewsPanel.php
@@ -14,6 +14,39 @@ class NewsPanel extends AbstractHelper
         return preg_replace($pattern, $replace, $text_to_search);
     }
     
+    private function stripHtml($text_to_search)
+    {
+        $text = preg_replace(
+            array(
+              // Remove invisible content
+                '@<head[^>]*?>.*?</head>@siu',
+                '@<style[^>]*?>.*?</style>@siu',
+                '@<script[^>]*?.*?</script>@siu',
+                '@<object[^>]*?.*?</object>@siu',
+                '@<embed[^>]*?.*?</embed>@siu',
+                '@<applet[^>]*?.*?</applet>@siu',
+                '@<noframes[^>]*?.*?</noframes>@siu',
+                '@<noscript[^>]*?.*?</noscript>@siu',
+                '@<noembed[^>]*?.*?</noembed>@siu',
+              // Add line breaks before and after blocks
+                '@</?((address)|(blockquote)|(center)|(del))@iu',
+                '@</?((div)|(h[1-9])|(ins)|(isindex)|(p)|(pre))@iu',
+                '@</?((dir)|(dl)|(dt)|(dd)|(li)|(menu)|(ol)|(ul))@iu',
+                '@</?((table)|(th)|(td)|(caption))@iu',
+                '@</?((form)|(button)|(fieldset)|(legend)|(input))@iu',
+                '@</?((label)|(select)|(optgroup)|(option)|(textarea))@iu',
+                '@</?((frameset)|(frame)|(iframe))@iu',
+            ),
+            array(
+                ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+                "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0", "\n\$0",
+                "\n\$0", "\n\$0",
+            ),
+            $text_to_search 
+        );
+        return strip_tags( $text );
+    }
+    
     public function __invoke($panelTitle, array $items, $panelClass = 'panel-default')
     {
         ?>
@@ -32,6 +65,7 @@ class NewsPanel extends AbstractHelper
             $bbCodeParser->parse($content);
             $content = $bbCodeParser->getAsHtml();
             $content = $this->stripBBCode($content);
+            $content = $this->stripHtml($content);
             ?>
 
                 <?php if (++$count == 1): ?>
