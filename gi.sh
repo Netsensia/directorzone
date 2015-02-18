bcaca3125fa5b47826ccd7027c269deb4547e22c
Chris Moreton <chris@netsensia.com>
2015-02-18 18:00:51 +0000

    Correct this damn image problem

--- gi.sh
+++ gi.sh
@@ -1,6 +1,6 @@
 ssh -i "/Volumes/Boxcryptor/Dropbox/Encrypted/Security/Keys/Amazon EC2/DirectorzoneSaltTrusty.pem" ubuntu@54.76.116.80 "sudo rm -rf /home/ubuntu/upload/* && sudo cp -R /var/www/directorzone/public/img/upload/* /home/ubuntu/upload && cd /home/ubuntu/upload && tar -zcvf upload.tar.gz *"
-scp -i "/Volumes/Boxcryptor/Dropbox/Encrypted/Security/Keys/Amazon EC2/DirectorzoneSaltTrusty.pem" ubuntu@54.76.116.80:upload.tar.gz /Users/Chris/git/directorzone/public/img
-cd /Users/Chris/git/directorzone/public/img
+scp -i "/Volumes/Boxcryptor/Dropbox/Encrypted/Security/Keys/Amazon EC2/DirectorzoneSaltTrusty.pem" ubuntu@54.76.116.80:upload/upload.tar.gz /Users/Chris/git/directorzone/public/img/upload
+cd /Users/Chris/git/directorzone/public/img/upload
 tar -zxvf upload.tar.gz
 rm upload.tar.gz
 
